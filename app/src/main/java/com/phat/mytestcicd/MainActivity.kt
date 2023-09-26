2859fb2280315bc86dd61a629b536d55a6e820be
phatbt <phatbui564@gmail.com>
2023-09-26 20:59:39 +0700

    up load project
    up load project

---
+++ app/src/main/java/com/phat/mytestcicd/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.phat.mytestcicd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
